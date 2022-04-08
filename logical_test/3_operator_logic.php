7039cee6dd3ea8e8418bc24f79daa13d62bc5d84
Putra R <putrarahmadi615@gmail.com>
2022-04-08 11:37:40 +0700

    update jawaban soal 3

--- logical_test/3_operator_logic.php
+++ logical_test/3_operator_logic.php
@@ -1,11 +1,17 @@
 <?php
 
-$bilangan1=7; //bilangan pertama yg dibagi
-$bilangan2=2; //bilangan kedua sebagai pembagi
-$hasil=0; //hasil pembagian
-$i=$bilangan2; //variabel untuk menyimpan jumlah perkalian
-while($i<=$bilangan1){
-    $i=$i+$bilangan2;
-    $hasil++;
+function hitung($a,$b){
+    $bilangan1=$a; //bilangan pertama yg dibagi
+    $bilangan2=$b; //bilangan kedua sebagai pembagi
+    $hasil=0; //hasil pembagian
+    $i=$bilangan2; //variabel untuk menyimpan jumlah perkalian
+    while($i<=$bilangan1){
+        $i=$i+$bilangan2;
+        $hasil++;
+    }
+    echo $bilangan1." : ".$bilangan2." = ".$hasil;
 }
-echo $hasil;
+
+hitung(7,2);
+echo "<br/>";
+hitung(8,4);
